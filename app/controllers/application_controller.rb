0ff58737924874167ec68d60e09028abd59fb014
masato.uno <masato.u20@gmail.com>
2019-09-30 14:24:00 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # 全てのコントローラに継承されているため
+  # ApplicationControllerで定義したメソッドは
+  # 全てのコントローラで使用できるようになる
+  include SessionsHelper
+
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
