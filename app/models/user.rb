6f424524bbd010e154cae49d6410f4377705208c
masato.uno <masato.u20@gmail.com>
2019-10-09 14:23:56 +0000

    favorite unfavorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
 
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -32,5 +35,21 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(micropost)
+    
+    p self.favorites
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorites = self.favorites.find_by(micropost_id: micropost.id)
+    favorites.destroy if favorites
+  end
+  
+  def favorite?(micropost)
+    self.favorite_microposts.include?(micropost)
+  end
+
   
 end
