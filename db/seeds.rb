daa298b8fdfae13d5426631b2b881ca45a70eef0
masato.uno <masato.u20@gmail.com>
2019-10-12 06:04:55 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+    user = User.new(name: 'user' + i.to_s, email: 'user' + i.to_s + '@user.com', password: 'user')
+    unless user.save
+      user  = User.find_by(name: 'user' + i.to_s)
+    end
+    p user
+    micropost = user.microposts.build(content: 'I\'m ' + user.name + '. hello!' , user_id: user.id )
+
+    micropost.save
+end
